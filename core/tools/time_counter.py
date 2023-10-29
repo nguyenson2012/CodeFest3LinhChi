8f18224301bdd735a69e07be807722f027775d60
quantrongtu <tuquan.fit.hanu@gmail.com>
2023-10-29 22:21:57 +0700

    This commit refactor about time - adding timeout checking to avoid dead-loop - refactor time measure tool to deal with different threads

--- core/tools/time_counter.py
+++ core/tools/time_counter.py
@@ -11,9 +11,9 @@ def measure_time(func):
     """
 
     def wrapper(*args, **kwargs):
-        start_time = time.time()
+        start_time = time.perf_counter()
         result = func(*args, **kwargs)
-        end_time = time.time()
+        end_time = time.perf_counter()
         execution_time = (end_time - start_time)
         print(f"Execution of {func.__name__} time: {execution_time:.6f} seconds")
         return result
